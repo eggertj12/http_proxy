52e364c1ef34ae38b7d87cf27e5060cc863fbb86
Eggert Jóhannesson <eggert@hugsaser.is>
2014-10-18 12:02:47 +0000

    IP resolving

--- http_proxy.py
+++ http_proxy.py
@@ -1,4 +1,5 @@
-from socket import *
+#from socket import *
+import socket
 import sys
 import select
 import threading
@@ -8,7 +9,7 @@ import threading
 # Will serve each connection and then close socket
 ###################################################
 
-def echoThread(socket, addr):
+def echoThread(connectionsocket, addr):
 
     # print debug info
     print 'connection from: ' + str(addr[0]) + ':' + str(addr[1])
@@ -18,38 +19,24 @@ def echoThread(socket, addr):
 
         # select blocks on list of sockets until reading / writing is available
         # or until timeout happens
-        readList, writeList, errorList = select.select([socket], [], [], 60)
+        readList, writeList, errorList = select.select([connectionsocket], [], [], 60)
 
         # empty list of sockets means a timeout occured
         if (len(readList) == 0):
-            peer = socket.getpeername()
+            peer = connectionsocket.getpeername()
             print 'connection closed after timeout: ' + str(peer[0]) + ':' + str(peer[1])
             break
-
-        # Socket was ready. Read message and check it's length
-        message = socket.recv(1024)
-        messageLength = len(message)
-
-        # A zero length message on a stream from select means it has been closed
-        if (messageLength == 0):
-            peer = socket.getpeername()
-            print 'connection closed by client: ' + str(peer[0]) + ':' + str(peer[1])
-            break
-
-        print message
+        packet, addr = connectionsocket.recvfrom(1024)
+#        requestname = (packet.split('\n'))[0]
+#        hostaddr = (packet.split(' '))[1]
+#        hostaddr2 = (packet.split(' '))[3]
+        hostaddr = (packet.split('\n')[1]).split(' ')[1]
+        hostipaddr = socket.gethostbyname(hostaddr.strip(' \t\n\r'))
         
-        # # In case of incomplete sending it needs to be wrapped in a loop
-        # # to make sure everything gets sent
-        # sentBytes = 0
-        # while (sentBytes < messageLength):
-        #     sent = socket.send(message[sentBytes:])
-
-        #     # Again 0 bytes through socket means it has closed, this time probably by some error
-        #     if (sent == 0):
-        #         print 'Error sending on connection.'
-        #         sys.exit(1)
-        #     sentBytes += sent
-
+#        print requestname
+        print 'hostaddress is: ' + hostaddr
+        print 'ipaddress is: ' + hostipaddr
+            
     # All work done for thread, close socket
     socket.close()
 
@@ -64,8 +51,8 @@ if (len(sys.argv) != 2):
 port = int(sys.argv[1])
 
 # Set up a listening socket for accepting connection
-listenSocket = socket(AF_INET, SOCK_STREAM)
-listenSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+listenSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listenSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listenSocket.bind(('', port))
 listenSocket.listen(1)
 
