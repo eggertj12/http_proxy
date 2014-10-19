962fcbebf67fb4b28dbb223e2beba2fee82e5968
Eggert Jóhannesson <eggert@hugsaser.is>
2014-10-19 17:04:18 +0000

    Changed reading headers

--- http_proxy.py
+++ http_proxy.py
@@ -8,40 +8,54 @@ import logging
 
 buflen = 4096
 
-class Request:
+class Message:
     """ Class to hold request info """
     def __init__(self):
         self.headers = {}
     
-def parse_request_line(buf, req):
-    sp = buf.split("\n", 1)
-#    print "request_line: ", sp[0]
-    req.verb, req.path, req.version = sp[0].split(" ")
-    return sp[1]
+def read_line_from_socket(s):
+    line = ""
+    c = ""
+    while c != "\n":
+        c = s.recv(1)
+        line = line + c
+    return line.strip(" \r\n\t")
 
-def parse_headers(buf, req):
-    line, buf = buf.split("\n", 1)
-    line = line.strip(" \n\r\t")
+def parse_request_line(s, req):
+    sp = read_line_from_socket(s)
+#    print "request_line: ", sp[0]
+    req.verb, req.path, req.version = sp.split(" ")
+    return
+
+def parse_response_line(s, resp):
+    sp = read_line_from_socket(s)
+    resp.version, resp.status, resp.text = sp.split(" ", 2)
+    return
+
+def parse_headers(s, req):
+    line = read_line_from_socket(s)
     while len(line) > 0:
 #       print "line: ", line
         splitted = line.split(':', 1)
         key = splitted[0].lower()
         value = splitted[1]
         req.headers[key] = value.strip(" \n\r\t")
-        splitted = buf.split("\n", 1)
-        line = splitted[0]
-        try:
-            buf = splitted[1]
-        except Exception:
-            buf = "\n"
-        # buf = buf.split("\n", 1)
-        line = line.strip(" \n\r\t")
+        line = read_line_from_socket(s)
 #       print buf
-    return buf
+    return
 
-def create_request(url):
-    response = requests.get(url)
-#    print "PRINTED RESPONSE " + response
+def create_request(req):
+    request = req.verb + " " + req.path + " " + req.version + "\r\n"
+    for header in req.headers:
+        request += header + ": " + req.headers[header] + "\r\n"
+    request = request + "\r\n"
+    return request
+
+def create_response(resp):
+    response = resp.version + " " + resp.status + " " + resp.text + "\r\n"
+    for header in resp.headers:
+        response += header + ": " + resp.headers[header] + "\r\n"
+    response = response + "\r\n"
     return response
 
 def get_dest_port(req):
@@ -57,10 +71,11 @@ def open_connection(req):
     return conn
 
 def log(req, response, addr):
-    log =  ': ' + str(addr[0]) + ':' + str(addr[1]) + ' ' + req.verb + ' ' + req.path + ' : ' \
-    + str(response.split()[1] + ' ' + str(response.split()[2]))
-    logging.basicConfig(filename=sys.argv[2], format='%(asctime)s %(message)s', datefmt='%Y-%m-%dT%H:%M:%S+0000')
-    logging.warning(log)
+    # log =  ': ' + str(addr[0]) + ':' + str(addr[1]) + ' ' + req.verb + ' ' + req.path + ' : ' \
+    # + str(response.split()[1] + ' ' + str(response.split()[2]))
+    # logging.basicConfig(filename=sys.argv[2], format='%(asctime)s %(message)s', datefmt='%Y-%m-%dT%H:%M:%S+0000')
+    # logging.warning(log)
+    pass
    
 
 ###################################################
@@ -76,25 +91,27 @@ def echoThread(connectionsocket, addr):
     # Loop to handle multiple messages, and messages bigger than buffer size
     while True:
 
-        # select blocks on list of sockets until reading / writing is available
-        # or until timeout happens
-        readList, writeList, errorList = select.select([connectionsocket], [], [], 30)
+        # # select blocks on list of sockets until reading / writing is available
+        # # or until timeout happens
+        # readList, writeList, errorList = select.select([connectionsocket], [], [], 30)
 
-        # empty list of sockets means a timeout occured
-        if (len(readList) == 0):
-            peer = connectionsocket.getpeername()
-            break
+        # # empty list of sockets means a timeout occured
+        # if (len(readList) == 0):
+        #     peer = connectionsocket.getpeername()
+        #     break
 
-        packet, addr1 = connectionsocket.recvfrom(buflen)
+#        packet, addr1 = connectionsocket.recvfrom(buflen)
 
         # length of 0 means connection was closed
-        if (len(packet) == 0):
-            break
 
-        req = Request()
+        # TODO: detect closecd connection
+        # if (len(packet) == 0):
+        #     break
 
-        trimmed = parse_request_line(packet, req)
-        trimmed = parse_headers(trimmed, req)
+        req = Message()
+
+        parse_request_line(connectionsocket, req)
+        parse_headers(connectionsocket, req)
 
         # host header is required
         if not ('host' in req.headers):
@@ -103,20 +120,50 @@ def echoThread(connectionsocket, addr):
 
         req.port = get_dest_port(req)
         connection = open_connection(req)
-        connection.send(packet)
-        response = connection.recv(buflen)
+
+        request_string = create_request(req)
+        print request_string
+        connection.send(request_string)
+        print "request sent"
+
+        # TODO: send rest of message if available
+
+        resp = Message()
+
+        parse_response_line(connection, resp)
+        print "response line read"
+        parse_headers(connection, resp)
+        print "headers read"
+
+        print resp.headers
+
+        response = create_response(resp)
+        print response
+
+        # print len(response)
+        # print "--------------------------------------------------"
+        # print response
+        # print "--------------------------------------------------"
 
         #Logging to file
-        log(req, response, addr)
+#        log(req, response, addr)
 
-        lengd = len(response)
+        lengd = int(resp.headers['content-length'])
+        print "lengd:", lengd
+
         #Used to fetch from response until all data has been sent
         connectionsocket.send(response)
-        if lengd == buflen:
-            while len(response)!= 0:
-                response = connection.recv(buflen)
-                connectionsocket.send(response)
-            
+
+        read = 0
+        while read < lengd:
+            response = connection.recv(buflen)
+            read = read + len(response)
+            connectionsocket.send(response)
+            print "len(response), read, content-length", len(response), int(read), int(lengd)
+
+        break
+
+    print "leaving thread"            
     # All work done for thread, close socket
     connectionsocket.close()
 
@@ -147,6 +194,7 @@ try:
         thr = threading.Thread(target=echoThread, args=(connectionSocket, addr))
         thr.daemon = True
         thr.start()
+#        echoThread(connectionSocket, addr)
 except timeout:
     print 'connection closed after timeout'
     
