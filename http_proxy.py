8b310918c3f3e0622b99fea7bc452c8d9d5f7277
Eggert Jóhannesson <eggert@hugsaser.is>
2014-10-24 20:49:22 +0000

    Add support for persistent connections and code cleanup

--- http_proxy.py
+++ http_proxy.py
@@ -9,20 +9,28 @@ from urlparse import urlsplit
 
 buflen = 4096
 
+# A simple class to hold request / response messages
 class Message:
     """ Class to hold request info """
     def __init__(self):
+        self.verb = ''
+        self.path = ''
+        self.version = ''
+        self.status = ''
+        self.text = ''
         self.headers = {}
-    
+
+# This is really unefficient it seems
+# TODO: implement a buffered version somehow
 def read_line_from_socket(s):
     line = ""
     c = ""
     while c != "\n":
-        sys.stdout.write(c)
         c = s.recv(1)
         line = line + c
     return line.strip(" \r\n\t")
 
+# Get the first line of a request and split into parts
 def parse_request_line(s, req):
     sp = read_line_from_socket(s)
     # print "request_line: ", sp
@@ -35,24 +43,25 @@ def parse_request_line(s, req):
         req.path += "#" + o.fragment
     return
 
+# Get the first line of a response and split into parts
 def parse_response_line(s, resp):
     sp = read_line_from_socket(s)
     # print "response_line: ", sp
     resp.version, resp.status, resp.text = sp.split(" ", 2)
     return
 
+# Read the headers line by line and store in dictionary
 def parse_headers(s, req):
     line = read_line_from_socket(s)
     while len(line) > 0:
-#       print "line: ", line
         splitted = line.split(':', 1)
         key = splitted[0].lower()
         value = splitted[1]
         req.headers[key] = value.strip(" \n\r\t")
         line = read_line_from_socket(s)
-#       print buf
     return
 
+# Rebuild the request to send to server
 def create_request(req):
     request = req.verb + " " + req.path + " " + req.version + "\r\n"
     for header in req.headers:
@@ -60,6 +69,7 @@ def create_request(req):
     request = request + "\r\n"
     return request
 
+# Rebuild the response received to send back to client
 def create_response(resp):
     response = resp.version + " " + resp.status + " " + resp.text + "\r\n"
     for header in resp.headers:
@@ -67,6 +77,7 @@ def create_response(resp):
     response = response + "\r\n"
     return response
 
+# Parse host value for port number. Defaults to 80 on not found
 def get_dest_port(req):
     if ':' in req.headers['host']:
         req.headers['host'], port = req.headers['host'].split(':')
@@ -74,11 +85,13 @@ def get_dest_port(req):
         port = '80'
     return port
 
+# Open a socket to the requested server
 def open_connection(req):
     conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     conn.connect((req.headers["host"], int(req.port)))
     return conn
 
+# Read data sent via buffered mode
 def read_content_length(reading, writing, length):
     read = 0
     while read < length:
@@ -86,29 +99,31 @@ def read_content_length(reading, writing, length):
         read = read + len(response)
         writing.send(response)
 
+# Read data sent via chunked transfer-encoding
+# This is primitive and does not support all features yet
 def read_chunked(reading, writing):
     chunk_line = read_line_from_socket(reading)
-    # print "chunk_line", repr(chunk_line)
     size = int(chunk_line.split(";", 1)[0], 16)
-    # print "size: ", repr(size)
 
     # TODO: check for trailing headers is chunk line
+
+    # Loop while there are chunks
     while size > 0:
-        read = 0
+        # Start by sending the chunk size
         writing.send(chunk_line + "\r\n")
-        # +2 to get trailing \r\n
+
+        read = 0
         while read < size:
             response = reading.recv(min(buflen, size - read))
             read = read + len(response)
             writing.send(response)
-            # print "size", repr(size), "read", repr(read)
+
+        # Try to read next chunk line, fall back to size zero in case server does not send terminating chunk
         chunk_line = read_line_from_socket(reading)
-        # print "chunk_line", repr(chunk_line)
         if len(chunk_line) > 0:
             size = int(chunk_line.split(";", 1)[0], 16)
         else:
             size = 0
-        # print "size: ", repr(size)
 
     # TODO: check for trailer-part
 
@@ -124,10 +139,51 @@ def read_chunked(reading, writing):
         line = read_line_from_socket(reading)
     return
 
-def is_persistent(req):
-    return False
+# Determine if the connection is to be kept alive
+def is_persistent(req, resp):
+    # First check if server wants to close connection
+    if (resp.version == 'HTTP/1.1'):
+        if ('connection' in resp.headers) and ('close' in resp.headers['connection']):
+            return False
 
+    elif (resp.version == 'HTTP/1.0'):
+        if not ('connection' in resp.headers and 'keep-alive' in resp.headers['connection']):
+            return False
+    
+    # Made it here, assume server wants a persistent connection
+    # Then check client request
+    if (resp.version == 'HTTP/1.1'):
+        if ('connection' in resp.headers) and ('close' in resp.headers['connection']):
+            return False
+        else:
+            return True
+
+    elif (resp.version == 'HTTP/1.0'):
+        if not ('connection' in resp.headers and 'keep-alive' in resp.headers['connection']):
+            return False
+        else:
+            return True
+        
+    # Unknown version, assume it is something newer than HTTP/1.1 and default to persistent
+    return True
+
+# Helper to create a response in case of error
+def create_error_response(req, status, message):
+    # set up a Message object for the logger
+    resp = Message()
+    if hasattr(req, 'version'):
+        resp.version = req.version
+    else:
+        resp.version = 'HTTP/1.1'
+    resp.status = status
+    resp.text = message
+    return resp
+
+# Write the request / response line to given log file
 def log(req, response, addr):
+    if not ('host' in req.headers):
+        req.headers['host'] = ''
+
     log =  ': ' + str(addr[0]) + ':' + str(addr[1]) + ' ' + req.verb + ' ' + req.headers['host'] + req.path + ' : ' \
     + response.status + ' ' + response.text
     logging.basicConfig(filename=sys.argv[2], format='%(asctime)s %(message)s', datefmt='%Y-%m-%dT%H:%M:%S+0000')
@@ -144,19 +200,17 @@ def connecion_handler(connectionsocket, addr):
     # print debug info
     print 'connection from: ' + str(addr[0]) + ':' + str(addr[1])
 
-    # Loop to handle multiple messages, and messages bigger than buffer size
+    # Loop to handle persistent connections
     while True:
 
         # select blocks on list of sockets until reading / writing is available
-        # or until timeout happens, set timeout of 5 seconds for persistent connections
-        readList, writeList, errorList = select.select([connectionsocket], [], [], 5)
+        # or until timeout happens, set timeout of 5 seconds for dropped connections
+        readList, writeList, errorList = select.select([connectionsocket], [], [], 30)
 
         # empty list of sockets means a timeout occured
         if (len(readList) == 0):
             peer = connectionsocket.getpeername()
             break
-
-#        packet, addr1 = connectionsocket.recvfrom(buflen)
 
         # length of 0 means connection was closed
 
@@ -164,47 +218,47 @@ def connecion_handler(connectionsocket, addr):
         # if (len(packet) == 0):
         #     break
 
+        # Create a message object for the request
         req = Message()
 
         parse_request_line(connectionsocket, req)
 
         # Only a small subset of requests are supported
         if not req.verb in ('GET', 'POST', 'HEAD'):
-            print 'unsupported http method'
+            resp = create_error_response(req, '405', 'Method Not Allowed')
+            connectionsocket.send(create_response(resp))
+            log(req, resp, addr)
+            # jump to cleanup
             break            
 
         parse_headers(connectionsocket, req)
 
         # host header is required
         if not ('host' in req.headers):
-            print 'invalid request'
-            break
+            resp = create_error_response(req, '400', 'Bad request')
+            connectionsocket.send(create_response(resp))
+            log(req, resp, addr)
+            break            
 
         req.port = get_dest_port(req)
 
         try:
             connection = open_connection(req)
 
+        # Get this gaierror if it is impossible to open a connection
+        # Blame it on the client and send a Bad request response
         except socket.gaierror, e:
-            response = 'HTTP/1.1 404 Not Found \n\n'
-            connectionsocket.send(response)
-
-            resp = Message()
-            resp.status = '404'
-            resp.text = 'Not Found'
-
-            #Logging to file
+            resp = create_error_response(req, '400', 'Bad request')
+            connectionsocket.send(create_response(resp))
             log(req, resp, addr)
 
             # Jump directly to cleanup
             break
 
         request_string = create_request(req)
-#        print request_string
         connection.send(request_string)
-#        print "request sent"
 
-        # TODO: send rest of message if available
+        # Send rest of message if available (POST data)
         if 'content-length' in req.headers:
             length = int(req.headers['content-length'])
             read_content_length(connectionsocket, connection, length)
@@ -215,38 +269,36 @@ def connecion_handler(connectionsocket, addr):
             if "chunked" in tf_encoding.lower():
                 read_chunked(connectionsocket, connection)
 
+        # Now on to handling the response
         resp = Message()
 
         parse_response_line(connection, resp)
         parse_headers(connection, resp)
 
-#        print resp.headers
-
         response = create_response(resp)
-#        print response
 
         #Logging to file
         log(req, resp, addr)
 
-        #Used to fetch from response until all data has been sent
+        # Send the response
         connectionsocket.send(response)
 
+        # Get the response data if any
         if 'content-length' in resp.headers:
             length = int(resp.headers['content-length'])
             read_content_length(connection, connectionsocket, length)
 
         elif 'transfer-encoding' in resp.headers:
             tf_encoding = resp.headers['transfer-encoding']
-            # print "transfer-encoding", tf_encoding
             if "chunked" in tf_encoding.lower():
                 read_chunked(connection, connectionsocket)
 
-        # TODO: persistent connection
-        if not is_persistent(req):
+        # Check if the connection should be kept alive, cleanup otherwise
+        if not is_persistent(req, resp):
             connection.close()
             break
 
-    print "leaving thread"            
+    # print "leaving thread"            
     # All work done for thread, close sockets
     connectionsocket.close()
 
@@ -261,18 +313,19 @@ if (len(sys.argv) != 3):
 
 port = int(sys.argv[1])
 
-threaded = False
+threaded = True
 
 # Set up a listening socket for accepting connection
 listenSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listenSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listenSocket.bind(('', port))
 try:
-    listenSocket.listen(20)
+    listenSocket.listen(5)
 
     # Then it's easy peasy from here on, just sit back and wait
     while True:
         connectionSocket, addr = listenSocket.accept()
+        # Does this make sense
         connectionSocket.settimeout(30)
 
         if threaded:
