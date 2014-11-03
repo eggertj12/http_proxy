4658171063cb7f1c57c5fffb8c836fa177a7cbbe
Lilja <liljaos11@ru.is>
2014-11-03 21:26:18 +0000

    Fixed date format in files

--- http_proxy.py
+++ http_proxy.py
@@ -219,12 +219,12 @@ def cache_file(url, filename, expire_date, data, content_type):
     lock = threading.Lock()
     my_dir = 'cache/' + str(url)
     with lock:
-        date = str.replace(str(eut.parsedate(expire_date)), ', ','_')
+        c = str(datetime.datetime.strptime(str(eut.parsedate(expire_date)), "(%Y, %m, %d, %H, %M, %S, 0, 1, -1)"))
+        date = str.replace(str.replace(str.replace(c, ':', ''), ' ', ''), '-', '')
         if not os.path.exists(my_dir):
             os.makedirs(my_dir)
         filename = filename.split("?")[0] + '|' + content_type
 
-        print "CACHING " + filename + "\n   FROM " + str(url)
         file = open(my_dir + '/' + date + urllib.quote_plus(filename), "ab+")
         file.write(data)
         file.close()
@@ -245,10 +245,9 @@ def is_in_cache(url, filename):
             s_file = file.split('%7C')[0]
             if s_file.endswith(searchfile):
                 currenttime = datetime.datetime.now()
-                filetime = datetime.datetime.strptime(str(s_file)[1:20], "%Y_%m_%d_%H_%M_%S")
+                filetime = datetime.datetime.strptime(str(s_file)[0:14], "%Y%m%d%H%M%S")
                 if currenttime < filetime:
                     myfile = mypath + file
-                    print "RETURNING " + myfile + "\n"
                     return myfile
 
         #searchfile = urllib.quote_plus(filename)[:29]
